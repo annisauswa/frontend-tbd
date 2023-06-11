4d0838e71446fe591faf5f732bada611bec1d976
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-06-11 10:21:02 +0700

    feat: add component book table

--- pages/book/index.js
+++ pages/book/index.js
@@ -1,80 +1,76 @@
-// import Link from "next/link";
+import { useState, useEffect } from "react";
+import Edit from "../../components/edit";
 
-// export const getStaticProps = async () => {
-//     const res = await fetch("http://localhost:3001/api/book");
-//     const data = await res.json();
+const Booktable = () => {
+    var heading = ["id", "Title", "Author", "Publication Year", "Pages", "Price", "Publisher", "Last Update", "DML"];
 
-//     return {
-//       props: { book: data },
-//     };
-//   };
+  const [books, setBooks] = useState([]);
 
-// export default function Book({ book }) {
-//     var heading = ["id", "Title", "Author", "Publication Year", "Pages", "Price", "Publisher", "Last Update", "DML"];
+  const getBook = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/api/book");
+      const jsonData = await res.json();
 
-    // const [books, setBooks] = useState([]);
+      setBooks(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
-    // const deleteTodo = async id => {
-    //     try {
-    //       const deleteTodo = await fetch(`http://localhost:3001/books/${id}`, {
-    //         method: "DELETE"
-    //       });
+  useEffect(() => {
+    getBook();
+  }, []);
+
+  console.log(books);
+
+    const deleteBook = async id => {
+        try {
+          const deleteBook = await fetch(`http://localhost:3001/book/${id}`, {
+            method: "DELETE"
+          });
     
-    //       setBooks(Books.filter(book => book.id !== id));
-    //     } catch (err) {
-    //       console.error(err.message);
-    //     }
-    //   };
+          setBooks(books.filter(book => book.id !== id));
+        } catch (err) {
+          console.error(err.message);
+        }
+      };
+  return (
+    <div>
+            <h1 className='text-black text-center font-bold text-xl'>DATA FROM BOOK TABLE</h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full table-pin-rows table-pin-cols text-black">
+                    <thead>
+                    <tr>
+                        {heading.map((head) => (
+                        <th key={head}>{head}</th>
+                        ))}
+                    </tr>
+                    </thead>
+                    <tbody className="">
+                    {books.map((item) => (
+                        <tr key={item.id}>
+                        <td>{item   .id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.auth_id}</td>
+                        <td>{item.publication_year}</td>
+                        <td>{item.pages}</td>
+                        <td>{item.price}</td>
+                        <td>{item.pub_id}</td>
+                        <td>{item.last_update}</td>
+                        <td>
+                            <Edit table={item} />
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => deleteBook(item.id)}>Delete
+                            </button>
+                        </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+          </div>
+  )
+}
 
-//     return(
-//         <div className="px-[200px] bg-white w-full min-h-screen">
-//         <h1 className=" text-black text-center">
-//           Good Reading Bookstore
-//         </h1>
-//         <div className="flex flex-col gap-[50px]">
-//           <div className="bg-red-400 rounded-[50px] w-full px-[100px] justify-around flex flex-row">
-//             <Link href="/book">
-//               <button className="hover:text-black focus:underline focus:text-black">BOOK</button>
-//             </Link>
-//             <Link href="/sqlbuilder">
-//               <div>SQL BUILDER</div>
-//             </Link>
-//           </div>
-//           <div className="overflow-x-auto">
-//             <table class="table w-full table-pin-rows table-pin-cols text-black">
-//                 <thead>
-//                 <tr>
-//                     {heading.map((head) => (
-//                     <th key={head}>{head}</th>
-//                     ))}
-//                 </tr>
-//                 </thead>
-//                 <tbody className="">
-//                 {book.map((item) => (
-//                     <tr key={item.id}>
-//                     <td>{item.id}</td>
-//                     <td>{item.title}</td>
-//                     <td>{item.auth_id}</td>
-//                     <td>{item.publication_year}</td>
-//                     <td>{item.pages}</td>
-//                     <td>{item.price}</td>
-//                     <td>{item.pub_id}</td>
-//                     <td>{item.last_update}</td>
-//                     <td>
-//                          <button className="btn btn-primary">Edit
-//                          </button>
-//                         <button
-//                             className="btn btn-danger"
-//                             onClick={() => deleteTodo(book.id)}>Delete
-//                         </button>
-//                     </td>
-//                     </tr>
-//                 ))}
-//                 </tbody>
-//             </table>
-//         </div>
-//         </div>
-//       </div>
-        
-//     )
-// }
+export default Booktable
